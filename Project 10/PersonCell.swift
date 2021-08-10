4d09d31c6d7ec9a10a821b044754f641f54bf1bf
Igor Polousov <igorpolousov@gmail.com>
2021-08-10 16:47:50 +0300

    + initial files and UI storyboard

---
+++ Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Igor Polousov on 10.08.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
