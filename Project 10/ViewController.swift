34a14be703479f62b45483f41af71a6cb8579332
Igor Polousov <igorpolousov@gmail.com>
2021-08-10 14:51:12 +0300

    Initial Commit

---
+++ Project 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 10
+//
+//  Created by Igor Polousov on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
