77ee6592716124372c66374aeae65a76a39ace35
Igor Polousov <igorpolousov@gmail.com>
2021-08-12 12:43:10 +0300

    + addPerson() with UIPicker + UIPickerControllerDelegate and UINavigationControllerDelegate  к классу , соотвествие этим протоколам нужно для выполнения метода addPerson() + imagePickerController() в котром были указаны: уникальное название изображения imageName, указан путь куда нужно записывать изображение при помощи imagePath с кастомные методом getDocumentsDirectory(), изображение было переведено в формат jpeg и далее записано по пути в imagePath + getDocumentsDirectory() в котором указан путь  к папке пользователя documents

--- Project 10/ViewController.swift
+++ Project 10/ViewController.swift
@@ -1,16 +1,17 @@
 //
 //  ViewController.swift
 //  Project 10
-//
+//  Day 42
 //  Created by Igor Polousov on 10.08.2021.
 //
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         
     }
     
@@ -26,6 +27,34 @@ class ViewController: UICollectionViewController {
         cell.name.text = "True "
         return cell
     }
+    
+    @objc func addNewPerson() {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+            
+            dismiss(animated: true)
+        }
+    }
 
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+        
+    }
+    
 }
 
